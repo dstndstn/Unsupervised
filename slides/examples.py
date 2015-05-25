2921d15cbf93c1e2a3066ae84f504a9cf09a93e4
Dustin Lang <dstndstn@gmail.com>
2015-05-25 13:48:20 -0400

    add GMM/E-M section

--- slides/examples.py
+++ slides/examples.py
@@ -141,7 +141,7 @@ def voronoi_plot_2d(vor, ax=None):
 
 
 def kmeans(ps, seed=None, getcluster=get_clusterA, K=3, N=200,
-           plotTruth=False, plotsymbol='.'):
+           plotTruth=False, truthOrder=None, plotsymbol='.'):
     C,ax = getcluster()
     X = sample_clusters(*C, N=N, bounds=np.array(ax).reshape(2,2))
     xi = X
@@ -225,7 +225,15 @@ def kmeans(ps, seed=None, getcluster=get_clusterA, K=3, N=200,
     if plotTruth:
         plt.clf()
         for i,x in enumerate(xi):
-            plt.plot(x[:,0], x[:,1], plotsymbol, mfc=colors[i], mec='k')
+            ii = i
+            if truthOrder is not None:
+                ii = truthOrder[i]
+            plt.plot(x[:,0], x[:,1], plotsymbol, mfc=colors[ii], mec='k')
+        for i,(a,mu,s) in enumerate(zip(*C)):
+            ii = i
+            if truthOrder is not None:
+                ii = truthOrder[i]
+            plt.plot(mu[0], mu[1], 'x', mew=2, ms=15, color=colors[ii])
         plt.axis(ax)
         plt.xticks([]); plt.yticks([])
         ps.savefig()
@@ -256,9 +264,171 @@ def kmeans_break1():
     # for i,xi in enumerate(X):
     #     plt.plot(xi[:,0], xi[:,1], '.', 
 
+def get_clusterC():
+    means = [ (3.5, 4.), (6.5, 4.) ]
+    stds  = [ 0.8, 0.5  ]
+    amps  = [ 0.9, 0.1 ]
+    ax = [0, 10, 0, 8]
+    return (amps, means, stds), ax
     
+def kmeans_break2():
+    ps = PlotSequence('break2', suffix='png')
+
+    np.random.seed(42)    
+    kmeans(ps, seed=None, getcluster=get_clusterC, K=2, plotTruth=True,
+           plotsymbol='o', truthOrder=[1,0], N=500)
+
+def sample_gmm(amps, means, covs, N=100, D=2, bounds=None):
+    K = len(amps)
+    amps = np.array(amps)
+    amps /= np.sum(amps)
+    nk = np.random.multinomial(N, amps)
+    x = []
+    trueclass = []
+    for n,mean,cov in zip(nk, means, covs):
+        u,s,v = np.linalg.svd(cov)
+        # angle = np.linspace(0., 2.*np.pi, 200)
+        # u1 = u[0,:]
+        # u2 = u[1,:]
+        # s1,s2 = np.sqrt(s)
+        # xy = (u1[np.newaxis,:] * s1 * np.cos(angle)[:,np.newaxis] +
+        #       u2[np.newaxis,:] * s2 * np.sin(angle)[:,np.newaxis])
+
+        xi = np.random.normal(size=(n,D))
+        print 'xi', xi.shape
+        xi = mean + np.dot(u, (xi * s).T).T
+        
+        if bounds is not None:
+            outofbounds = np.empty(len(xi), bool)
+            while True:
+                outofbounds[:] = False
+                for d,(lo,hi) in enumerate(bounds):
+                    outofbounds |= np.logical_or(xi[:,d] < lo, xi[:,d] > hi)
+                if not np.any(outofbounds):
+                    break
+                I = np.flatnonzero(outofbounds)
+                newxi = np.random.normal(size=(len(I),D))
+                xi[I,:] = mean + np.dot(u, (newxi * s).T).T
+            
+        x.append(xi)
+    print [xi.shape for xi in x]
+    return x
+
+def gaussian_probability(X, mean, cov):
+    D,d = cov.shape
+    #print 'X', X.shape
+    d1 = np.dot(np.linalg.inv(cov), (X - mean).T)
+    #print 'd1', d1.shape
+    mahal = np.sum(d1.T * (X - mean), axis=1)
+    #d2 = np.dot((X - mean).T, d1.T)
+    #print 'd2', d2.shape
+    #print 'mahal', mahal.shape
+    #mahal = np.dot((X - mean.T), np.dot(np.linalg.inv(cov), (X - mean).T).T)
+    return (1./((2.*np.pi)**(D/2.) * np.sqrt(np.linalg.det(cov)))
+            * np.exp(-0.5 * mahal))
+    
+def gmm1():
+    amps = [0.8, 0.2]
+    means = [ (3., 4.), (6.5, 4.) ]
+    covs = [ np.array([[1.,-0.5],[-0.5,1.]]),
+             np.array([[1.,0.5],[0.5,1.]]),
+             ]
+    N = 1000
+    X = sample_gmm(amps, means, covs, N=N)
+
+    ps = PlotSequence('gmm', suffix='pdf')
+
+    order = [1,0]
+    
+    plt.clf()
+    for i,xi in enumerate(X):
+        plt.plot(xi[:,0], xi[:,1], '.', color=colors[order[i]])
+    # plt.axis('scaled')
+    plt.axis('equal')
+    ps.savefig()
+    ax = plt.axis()
+
+    #ax = [0, 10, 0, 8]
+    
+    K = 2
+
+    amps = np.ones(K) / K
+    means = np.random.normal(size=(K,2)) * 2
+    means[:,0] += (ax[0]+ax[1])/2.
+    means[:,1] += (ax[2]+ax[3])/2.
+    covs = [np.eye(2) for i in range(K)]
+
+    Xi = X
+    X = np.vstack(X)
+    
+    for i in range(25):
+        z = np.zeros((K,N))
+        for k,(amp,mean,cov) in enumerate(zip(amps, means, covs)):
+
+            print 'Component K: amp', amp, 'mean', mean, 'cov', cov
+
+            z[k,:] = amp * gaussian_probability(X, mean, cov)
+
+        # plt.clf()
+        # plt.plot(X[:,0], X[:,1], 'k.')
+        # for k,(amp,mean,cov) in enumerate(zip(amps, means, covs)):
+        #     u,s,v = np.linalg.svd(cov)
+        #     angle = np.linspace(0., 2.*np.pi, 200)
+        #     u1 = u[0,:]
+        #     u2 = u[1,:]
+        #     s1,s2 = np.sqrt(s)
+        #     xy = (u1[np.newaxis,:] * s1 * np.cos(angle)[:,np.newaxis] +
+        #           u2[np.newaxis,:] * s2 * np.sin(angle)[:,np.newaxis])
+        #     plt.plot(mean[0] + xy[:,0], mean[1] + xy[:,1], '-', color=colors[k], lw=2)
+        # zmax = np.argmax(z, axis=0)
+        # print 'zmax', zmax.shape
+        # 
+        # for k in range(K):
+        #     I = np.flatnonzero(zmax == k)
+        #     plt.plot(X[I,0], X[I,1], 'o', mec=colors[k], mfc='none')
+        # 
+        # ps.savefig()
+
+        plt.clf()
+        # snazzy red/blue color coding
+        cc = np.zeros((N,3))
+        cc[:,2] = z[0,:] / np.sum(z, axis=0)
+        cc[:,0] = z[1,:] / np.sum(z, axis=0)
+        plt.scatter(X[:,0], X[:,1], color=cc, s=9, alpha=0.5)
+        #plt.plot(X[:,0], X[:,1], 'o', mec='k', mfc='none', ms=3, alpha=0.1)
+
+        for k,(amp,mean,cov) in enumerate(zip(amps, means, covs)):
+            u,s,v = np.linalg.svd(cov)
+            angle = np.linspace(0., 2.*np.pi, 200)
+            u1 = u[0,:]
+            u2 = u[1,:]
+            s1,s2 = np.sqrt(s)
+            xy = (u1[np.newaxis,:] * s1 * np.cos(angle)[:,np.newaxis] +
+                  u2[np.newaxis,:] * s2 * np.sin(angle)[:,np.newaxis])
+            plt.plot(mean[0] + xy[:,0], mean[1] + xy[:,1], 'k-', lw=4)
+            plt.plot(mean[0] + xy[:,0], mean[1] + xy[:,1], '-', color=colors[k], lw=2)
+
+        plt.axis(ax)
+        ps.savefig()
+
+        z /= np.sum(z, axis=0)
+
+        newamps = np.sum(z, axis=1)
+        newamps /= np.sum(newamps)
+        print 'new amps', newamps
+        newmeans = [np.sum(z[k,:][np.newaxis,:] * X.T, axis=1) / np.sum(z[k,:])
+                    for k in range(K)]
+        print 'new means', newmeans
+        newcovs = [np.dot(z[k,:] * (X - mean).T, X - mean) / np.sum(z[k,:])
+                   for k,mean in enumerate(means)]
+        print 'new covs', newcovs
+
+        amps = newamps
+        means = newmeans
+        covs = newcovs
         
 
+    
 plt.figure(figsize=(4,3))
 plt.subplots_adjust(left=0.1, right=0.98, bottom=0.1, top=0.95)
     
@@ -277,6 +447,10 @@ plt.subplots_adjust(left=0.1, right=0.98, bottom=0.1, top=0.95)
 # os.system('avconv -r 4 -y -i kmeans2-%02d.png kmeans2.mov')
 
 plt.subplots_adjust(left=0.01, right=0.99, bottom=0.01, top=0.99)
-kmeans_break1()
+#kmeans_break1()
+#kmeans_break2()
 
+np.random.seed(42)    
+
+gmm1()
 
